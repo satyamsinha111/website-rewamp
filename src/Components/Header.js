e2c111a19312c7b974fd5c910b133b8b0e7fbbf9
satyamsinha111 <satyamsinha8158@gmail.com>
2022-05-08 14:34:18 +0530

    initial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,7 +31,7 @@ function Header() {
     <>
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6 select-none">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <Link to="/">
+          <Link to="/" onClick={toggleMenu}>
             <span className="font-semibold text-xl tracking-tight">
               Satyam sinha
             </span>
