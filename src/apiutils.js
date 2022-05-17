4bb0d5a877a5b2f8595dff6881bfbfde5036f80a
satyamsinha111 <satyamsinha8158@gmail.com>
2022-05-17 20:50:33 +0530

    pushed

--- src/apiutils.js
+++ src/apiutils.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-let baseurl = "https://satyamsinha.online/api/client";
+let baseurl = "https://satyamsinha.online/client";
 export const sendMessage = (data) => {
   return axios.post(`${baseurl}/client`, data);
 };
